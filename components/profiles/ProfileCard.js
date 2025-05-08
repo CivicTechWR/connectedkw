6a81abd4f0099a9f29f22070b930252abc22faa3
MarvelousChester <karandeepsandhu2789@gmail.com>
2025-05-08 15:48:24 -0400

    Add "..." more icon to indicate more skills on profile card

--- components/profiles/ProfileCard.js
+++ components/profiles/ProfileCard.js
@@ -38,20 +38,40 @@ function getRandomColor(skillName) {
 
 function renderSkills(profileSkillNames) {
   const maxSkillsToShow = 5; // Limit the number of skills displayed
+  
+  // If there are more than 5 skills, do a ... that will just open there page to show all the skills
 
   if (profileSkillNames.length === 0) {
     return <span className="text-xs text-gray-500">No skills</span>
   }
 
-  return profileSkillNames.slice(0, maxSkillsToShow).map((skillName, index) => (
-    <span
-      key={index}
-      style={{ backgroundColor: getRandomColor(skillName.name) }}
-      className="inline-flex items-center text-white px-2 py-1 text-xs font-semibold rounded-full"
-    >
-      {skillName.name}
-    </span>
-  ))
+  return (
+    <>
+      {profileSkillNames.slice(0, maxSkillsToShow).map((skillName, index) => (
+        <span
+          key={index}
+          style={{ backgroundColor: getRandomColor(skillName.name) }}
+          className="inline-flex items-center text-white px-2 py-1 text-xs font-semibold rounded-full"
+        >
+          {skillName.name}
+        </span>
+      ))}
+      {profileSkillNames.length > maxSkillsToShow && (
+        <span
+          className="inline-flex items-center justify-center w-12 h-6 bg-gray-300 rounded-full"
+          title="Show more skills"
+        >
+          <Image
+            src="/icons/more-horizontal.svg"
+            alt="Show more"
+            className="w-4 h-4" 
+            width={4}
+            height={4}
+          />
+        </span>
+      )}
+    </>
+  )
 }
 
 function renderAvatar(avatarURL, profileName) {
