e964d8077306289ee76db22cd09ed0b0e35fd9b2
MarvelousChester <karandeep2789@gmail.com>
2025-05-14 18:38:35 -0400

    Fix missing background for skills

--- components/profiles/RenderSkills.js
+++ components/profiles/RenderSkills.js
@@ -54,48 +54,50 @@ export default function RenderProfileSkills({ profileSkillNames }) {
     : profileSkillNames.slice(0, maxSkillsToShow);
   return (
     <>
-      {skillsToDisplay.map((skillName, index) => (
-        <span
-          key={index}
-          style={{ backgroundColor: getRandomColor(skillName.name) }}
-          className="inline-flex items-center text-white px-2 py-1 text-xs font-semibold rounded-full mr-2 mb-1"
-        >
-          {skillName.name}
-        </span>
-      ))}
-      {profileSkillNames.length > maxSkillsToShow && (
-        <>
-          {!showAll ? (
-            <button
-              className="flex items-center justify-center ml-auto w-12 h-6 bg-gray-300 rounded-full cursor-pointer"
-              title="Show more skills"
-              onClick={() => setShowAll(true)}
-            >
-              <Image
-                src="/icons/more-horizontal.svg"
-                alt="Show more"
-                className="w-4 h-4"
-                width={4}
-                height={4}
-              />
-            </button>
-          ) : (
-            <button
-              className="flex items-center justify-center ml-auto w-12 h-6 bg-gray-300 rounded-full cursor-pointer"
-              title="Show less skills"
-              onClick={() => setShowAll(false)}
-            >
-              <Image
-                src="/icons/minus.svg"
-                alt="Show less"
-                className="w-4 h-4"
-                width={4}
-                height={4}
-              />
-            </button>
-          )}
-        </>
-      )}
+      <div className="rounded-xl overflow-hidden shadow-lg bg-white p-5 mt-4">
+        {skillsToDisplay.map((skillName, index) => (
+          <span
+            key={index}
+            style={{ backgroundColor: getRandomColor(skillName.name) }}
+            className="inline-flex bg-white items-center text-white px-2 py-1 text-xs font-semibold rounded-full mr-2 mb-1"
+          >
+            {skillName.name}
+          </span>
+        ))}
+        {profileSkillNames.length > maxSkillsToShow && (
+          <>
+            {!showAll ? (
+              <button
+                className="flex items-center justify-center ml-auto w-12 h-6 bg-gray-300 rounded-full cursor-pointer"
+                title="Show more skills"
+                onClick={() => setShowAll(true)}
+              >
+                <Image
+                  src="/icons/more-horizontal.svg"
+                  alt="Show more"
+                  className="w-4 h-4"
+                  width={4}
+                  height={4}
+                />
+              </button>
+            ) : (
+              <button
+                className="flex items-center justify-center ml-auto w-12 h-6 bg-gray-300 rounded-full cursor-pointer"
+                title="Show less skills"
+                onClick={() => setShowAll(false)}
+              >
+                <Image
+                  src="/icons/minus.svg"
+                  alt="Show less"
+                  className="w-4 h-4"
+                  width={4}
+                  height={4}
+                />
+              </button>
+            )}
+          </>
+        )}
+      </div>
     </>
   );
 }
