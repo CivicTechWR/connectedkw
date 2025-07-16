1aabcff2693a142953dbcc700056414178f680d7
ian-cawood <ian.cawood@gmail.com>
2025-07-16 19:42:17 -0400

    chore(backend): create fsa endpoint

--- app/one-million-neighbours/page.js
+++ app/one-million-neighbours/page.js
@@ -1,6 +1,9 @@
 import FsaMap from "components/maps/FsaMap"
+import { getFSAs } from 'integrations/directus';
 
-export default function OneMillionNeighboursPage() {
+export default async function OneMillionNeighboursPage() {
+  const FSAs = await getFSAs()
+  console.log(FSAs)
   return (
       <div className="container mx-auto px-4 py-16">
         <h1 className="text-4xl font-bold mb-8">One Million Neighbours</h1>
