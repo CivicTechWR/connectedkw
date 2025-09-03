c4b53fdd4d6e08b1afa7dd663a524190ab628184
Jessica Liu <jess@Jessicas-Laptop.local>
2025-09-03 19:38:51 -0400

    clean up page

--- app/one-million-neighbours/page.js
+++ app/one-million-neighbours/page.js
@@ -1,23 +1,8 @@
-import { getFSAGeoData, getFSAData } from 'integrations/directus';
 import OneMillionNeighboursComponent from 'components/OneMillionNeighboursComponent';
-
-const defaultGeoJSON = {
-  "type": "FeatureCollection",
-  "crs": {
-    "type": "name",
-    "properties": {
-      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-    }
-  },
-  "name": "fsa_2021_reprojected",
-  "features": []
-}
 
 export default async function OneMillionNeighboursPage() {
   const FSAGeoData = await getFSAGeoData()
   const FSAData = await getFSAData()
-
-
 
   return (
     <div className="container mx-auto px-4 py-16">
